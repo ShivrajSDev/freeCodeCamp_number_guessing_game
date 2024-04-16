a225eb7ff1f61aed3b683fd05ba58fd62837f6e8
Shiv S <shivrajsdev@gmail.com>
2024-04-16 10:23:01 +0000

    feat: Update shell script logic

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
-echo $SECRET_NUMBER
 
 echo Enter your username:
 read NAME
@@ -9,8 +8,9 @@ read NAME
 USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$NAME';")
 if [[ -z $USER_ID ]]
 then
-  INSERT_USER_RESULT=$($PSQL "INSERT INTO users (name) VALUES ('$NAME');")
   echo "Welcome, $NAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users (name) VALUES ('$NAME');")
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE name='$NAME';")
 else
   GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE user_id='$USER_ID';")
   BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE user_id='$USER_ID';")
@@ -19,13 +19,13 @@ fi
 
 echo Guess the secret number between 1 and 1000:
 USER_NUMBER=0
-NUM_TRIES=0
+NUM_GUESSES=0
 until [[ $USER_NUMBER =~ $SECRET_NUMBER ]]
 do
   read USER_NUMBER
   if [[ $USER_NUMBER =~ ^[0-9]+$ ]]
   then
-    NUM_TRIES=$(($NUM_TRIES+1))
+    NUM_GUESSES=$(($NUM_GUESSES+1))
     if [[ $USER_NUMBER -gt $SECRET_NUMBER ]]
     then
       echo "It's lower than that, guess again:"
@@ -40,4 +40,12 @@ do
   fi
 done
 
-echo "You guessed it in $NUM_TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+GAMES_PLAYED=$(($GAMES_PLAYED+1))
+UPDATE_USER_GAMES_PLAYED_RESULT=$($PSQL "UPDATE users SET games_played=$GAMES_PLAYED WHERE user_id=$USER_ID;")
+if [[ -z $BEST_GAME || $BEST_GAME -gt $NUM_GUESSES ]]
+then
+  BEST_GAME=$(($NUM_GUESSES))
+  UPDATE_USER_BEST_GAME_RESULT=$($PSQL "UPDATE users SET best_game=$BEST_GAME WHERE user_id=$USER_ID;")
+fi
+
+echo "You guessed it in $NUM_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
